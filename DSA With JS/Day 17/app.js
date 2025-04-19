0deeb0b27ddeb21112048fb825a8cb80e4879784
Atish Adhav <adhavatish04@gmail.com>
2025-04-19 17:04:12 +0530

    Added Questions on String

--- DSA With JS/Day 17/app.js
+++ DSA With JS/Day 17/app.js
@@ -22,23 +22,54 @@ const prompt = require("prompt-sync")()
 // }
 
 
-// Q. 66) Toggle each alphabet of string
-let s = prompt('Enter a string : ');
-console.log(`The original string : ${s}`);
+// // Q. 66) Toggle each alphabet of string
+// let s = prompt('Enter a string : ');
+// console.log(`The original string : ${s}`);
 
+// let ans = "";
+// for(let i=0; i<s.length; i++){
+//     let asci = s.charCodeAt(i);
+
+//     // Upper case to lowercase
+//     if(asci>=65 && asci<=90){
+//         ans = ans + String.fromCharCode(asci + 32);
+//     }
+
+//     // lowercase to uppercase
+//     else if(asci>=97 && asci<=122){
+//         ans = ans + String.fromCharCode(asci - 32);
+//     }
+// }
+
+// console.log(`The toggle String : ${ans}`);
+
+// // Q. 67) Take an array of strings words and a string prefix. Print the number of strings in words that contain pref as prefix
+// //        words = ["pay", "attention", "practice", "attend"];
+
+// let words = prompt("Enter the words : ").split(" ");
+// let pref = "at";
+// let count = 0;
+
+// for(let i=0; i<words.length; i++){
+//     if(words[i].startsWith(pref)){
+//         count++;
+//     }
+// }
+
+// console.log(count);
+
+// Q. 68) Capatalize first and last character ch of each word in the sentence and print the new sentence
+let s = prompt("Enter a sentence : ").split(" ");
+console.log(`Original sentence : ${s}`);
 let ans = "";
+
 for(let i=0; i<s.length; i++){
-    let asci = s.charCodeAt(i);
-
-    // Upper case to lowercase
-    if(asci>=65 && asci<=90){
-        ans = ans + String.fromCharCode(asci + 32);
+    let word = s[i];
+    if(word.length<=2){
+        ans = ans + word.toUpperCase()+" ";
     }
-
-    // lowercase to uppercase
-    else if(asci>=97 && asci<=122){
-        ans = ans + String.fromCharCode(asci - 32);
+    else{
+        ans = ans + word.charAt(0).toUpperCase() + word.substring(1, word.length-1) + word.charAt(word.length-1).toUpperCase()+" ";
     }
 }
-
-console.log(`The toggle String : ${ans}`);
+console.log(`New sentence : ${ans}`);
